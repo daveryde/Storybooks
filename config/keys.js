bc2abbee4355c834c8de3459e62a4f0ab7dfda4e
David Cordova <d.mcordova87@gmail.com>
2018-08-01 21:40:30 -0700

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://daveryde:Stupid001!@ds163781.mlab.com:63781/storybooks-dev',
+    googleClientID:'263278705677-b17iqevlvvsav40s8u8ipqqvn5ebnqh4.apps.googleusercontent.com',
+    googleClientSecret:'4wGqOxiWjpX2OEkvgm8lbwym'
+}
