b4905a37efff911c988fb2c08d5c70d417b31dd7
David Cordova <d.mcordova87@gmail.com>
2018-08-05 11:04:35 -0700

    implemented edit and delete UI and functionality

--- routes/stories.js
+++ routes/stories.js
@@ -1,62 +1,104 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Story = mongoose.model('stories');
-const User = mongoose.model('users');
-const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
+const mongoose = require("mongoose");
+const Story = mongoose.model("stories");
+const User = mongoose.model("users");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
 // Stories Index
-router.get('/', (req, res) => {
-    Story.find({status:'public'})
-    .populate('user')
+router.get("/", (req, res) => {
+  Story.find({ status: "public" })
+    .populate("user")
     .then(stories => {
-        res.render('stories/index', {
-            stories: stories
-        });
+      res.render("stories/index", {
+        stories: stories
+      });
     });
 });
 
 // Show Single Story
-router.get('/show/:id', (req, res) => {
-    Story.findOne({
-        _id: req.params.id
-    })
-    .populate('user')
+router.get("/show/:id", (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+    .populate("user")
     .then(story => {
-        res.render('stories/show', {
-            story: story
-        });
+      res.render("stories/show", {
+        story: story
+      });
     });
 });
 
 // Add Story Form
-router.get('/add', ensureAuthenticated, (req, res) => {
-    res.render('stories/add');
+router.get("/add", ensureAuthenticated, (req, res) => {
+  res.render("stories/add");
+});
+
+// Edit Story Form
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  }).then(story => {
+    res.render("stories/edit", {
+      story: story
+    });
+  });
 });
 
 // Process Add Story
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  let allowComments;
+
+  if (req.body.allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: allowComments,
+    user: req.user.id
+  };
+
+  // Create Story
+  new Story(newStory).save().then(story => {
+    res.redirect(`/stories/show/${story.id}`);
+  });
+});
+
+// Edit Form Process
+router.put("/:id", (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  }).then(story => {
     let allowComments;
 
-    if(req.body.allowComments){
-        allowComments = true;
+    if (req.body.allowComments) {
+      allowComments = true;
     } else {
-        allowComments = false;
+      allowComments = false;
     }
 
-    const newStory = {
-        title: req.body.title,
-        body: req.body.body,
-        status: req.body.status,
-        allowComments: allowComments,
-        user: req.user.id
-    }
+    // New Values
+    story.title = req.body.title;
+    story.body = req.body.title;
+    story.status = req.body.status;
+    story.allowComments = allowComments;
 
-    // Create Story
-    new Story(newStory)
-    .save()
-    .then(story => {
-        res.redirect(`/stories/show/${story.id}`);
+    story.save().then(story => {
+      res.redirect("/dashboard");
+    });
+  });
+});
+
+// Delete Story
+router.delete('/:id', (req, res) => {
+    Story.remove({_id: req.params.id})
+    .then(() => {
+        res.redirect('/dashboard');
     });
 });
 
